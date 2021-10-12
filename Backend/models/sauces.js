314ed3451ac89cb41a8d3cd94bbe934c6db62127
boom <jeanmar-97.1@hotmail.fr>
2021-10-12 14:25:05 +0200

    modification du modèle sauce pour son bon fonctionnement

--- Backend/models/sauces.js
+++ Backend/models/sauces.js
@@ -7,10 +7,6 @@ const saucesSchema = mongoose.Schema( {
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
-    usersLiked: [ { type: String, required: true } ],
-    usersDisliked: [ { type: String, required: true } ],
 } );
 
-module.exports = mongoose.model( 'Sauces', saucesSchema );
+module.exports = mongoose.model( 'sauces', saucesSchema );
