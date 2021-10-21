fedf29277604f46c076902bb4a9d564070c1b940
boom <jeanmar-97.1@hotmail.fr>
2021-10-21 18:03:28 +0200

    ajout d'exress session

--- Backend/app.js
+++ Backend/app.js
@@ -40,6 +40,20 @@ const limiter = rateLimit( {
     max: 100 // limit each IP to 100 requests per windowMs
 } );
 
+//EXPRESS-SESSION, CHANGE LE NOM DU COOKIE//
+var session = require( 'express-session' );
+app.set( 'trust proxy', 1 ) // trust first proxy
+app.use( session( {
+    secret: 's3Cur3',
+    name: 'sessionId',
+} )
+);
+
+
+
+
+
+
 //  apply to all requests
 app.use( limiter );
 
